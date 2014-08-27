2b653a534d70e6fdd2d4690406db72fa8f473950
Thomas Belliard <thomas.belliard@sylogix.net>
2014-08-27 15:50:06 +0300

    Publishing version 0.0.10

--- lowcostexpress.php
+++ lowcostexpress.php
@@ -56,7 +56,7 @@ class LowCostExpress extends CarrierModule
   {
     $this->name = 'lowcostexpress';
     $this->tab = 'shipping_logistics';
-    $this->version = '0.0.9';
+    $this->version = '0.0.10';
     $this->author = 'Low Cost Express SAS';
 
     parent::__construct();
