ed9f8b71e4876aa6e3a30478aaab0575ae0b3e57
Thomas Belliard <thomas.belliard@sylogix.net>
2016-10-24 23:04:24 +0300

    A couple more changes for PS validator compatibility

--- controllers/admin/adminshipment.php
+++ controllers/admin/adminshipment.php
@@ -24,6 +24,7 @@
 *
 *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 */
+
 if (!defined('_PS_VERSION_')) {
     exit;
 }
--- models/LceShipment.php
+++ models/LceShipment.php
@@ -22,6 +22,7 @@
  *
  * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
+
 class LceShipment extends ObjectModel
 {
     public $id_shipment;
