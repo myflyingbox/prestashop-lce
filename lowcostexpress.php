d923bda96b08e0ab9df59598ea9386e4d806fe68
Thomas Belliard <thomas.belliard@sylogix.net>
2017-03-08 15:20:18 +0200

    This branch is not compatible with Prestashop 1.7

--- lowcostexpress.php
+++ lowcostexpress.php
@@ -96,7 +96,7 @@ class LowCostExpress extends CarrierModule
 
         $this->displayName = $this->l('MY FLYING BOX Express Shipping');
         $this->description = $this->l('Your shipments made easy with major express carriers (DHL, UPS, Chronopost...) at competitive negotiated rates.');
-        $this->ps_versions_compliancy = array('min' => '1.5', 'max' => _PS_VERSION_);
+        $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.6');
         $this->module_key = '5100c5ae613ddfacd4bc468aee7ee59e';
 
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall this module?');
